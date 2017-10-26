d4d36555604a7aa2c4bb1255955466289838c29f
Georg Dangl <dangl@iabi.eu>
2017-10-26 11:21:48 +0200

    Add printer classes for test listing

---
+++ src/iabi.bCertApi.Console/ModelViewDefinitionsPrinter.cs
@@ -0,0 +1,35 @@
+﻿using iabi.bCertApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace iabi.bCertApi.Console
+{
+    public class ModelViewDefinitionsPrinter
+    {
+        private readonly IEnumerable<ModelViewDefinition> _modelViewDefinitions;
+        private int _padName;
+        private const int _indentSpaces = 4;
+
+        public ModelViewDefinitionsPrinter(IEnumerable<ModelViewDefinition> modelViewDefinitions)
+        {
+            _modelViewDefinitions = modelViewDefinitions;
+            GetPaddings();
+        }
+
+        private void GetPaddings()
+        {
+            _padName = _modelViewDefinitions.Select(m => m.Name?.Length ?? 0).Max();
+        }
+
+        public void PrintModelViewDefinitions(IEnumerable<ModelViewDefinition> mvds)
+        {
+            var indent = new string(' ', _indentSpaces);
+            System.Console.WriteLine(indent + "Model View Definitions:");
+            foreach (var mvd in mvds)
+            {
+                var name = mvd.Name.PadRight(_padName, ' ');
+                System.Console.WriteLine(indent + $"{name}, Id: {mvd.Id}");
+            }
+        }
+    }
+}
---
+++ src/iabi.bCertApi.Console/TestsPrinter.cs
@@ -0,0 +1,44 @@
+﻿using iabi.bCertApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace iabi.bCertApi.Console
+{
+    public class TestsPrinter
+    {
+        private int _padName;
+        private readonly IList<Test> _tests;
+        private readonly ModelViewDefinitionsPrinter _modelViewDefinitionsPrinter;
+
+        public TestsPrinter(IList<Test> tests)
+        {
+            _tests = tests;
+            var allMvds = tests.SelectMany(t => t.ModelViewDefinitions);
+            _modelViewDefinitionsPrinter = new ModelViewDefinitionsPrinter(allMvds);
+            GetPaddings();
+        }
+
+        private void GetPaddings()
+        {
+            _padName = _tests.Select(t => t.Name?.Length ?? 0).Max();
+        }
+
+        private void PrintSingleTest(Test test)
+        {
+            var name = test.Name.PadRight(_padName, ' ');
+            var erName = test.ExchangeRequirementName;
+            System.Console.WriteLine($"Test: {name}, Exchange Requirement: {erName}");
+            var mvds = test.ModelViewDefinitions;
+            _modelViewDefinitionsPrinter.PrintModelViewDefinitions(mvds);
+            System.Console.WriteLine("--------------------");
+        }
+
+        public void PrintTests()
+        {
+            foreach (var test in _tests)
+            {
+                PrintSingleTest(test);
+            }
+        }
+    }
+}
