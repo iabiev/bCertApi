b1a2fd5b10a8564754f510d141865b7d66539bef
Georg Dangl <dangl@iabi.eu>
2017-10-26 11:22:38 +0200

    Implement program

--- src/iabi.bCertApi.Console/Program.cs
+++ src/iabi.bCertApi.Console/Program.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using CommandLine.Text;
+using System;
+using System.Threading.Tasks;
 
 namespace iabi.bCertApi.Console
 {
     class Program
     {
-        static void Main(string[] args)
+        static Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            return RunProgram(args);
+        }
+
+        private static async Task RunProgram(string[] args)
+        {
+            var optionsParser = new OptionsParser(args);
+            if (optionsParser.IsValid)
+            {
+                var options = optionsParser.Result;
+                System.Console.WriteLine(HeadingInfo.Default);
+                System.Console.WriteLine(CopyrightInfo.Default);
+                try
+                {
+                    var defaultHttpHandler = new bCertDefaultHttpHandler(options.ApiKey, options.BaseUri);
+                    if (options.ListTests)
+                    {
+                        var testCaseLister = new TestCaseLister(options, defaultHttpHandler);
+                        await testCaseLister.ListTestCasesAsync();
+                    }
+                    else
+                    {
+                        var checker = new Checker(options, defaultHttpHandler);
+                        await checker.CheckFile();
+                    }
+                }
+                catch (Exception e)
+                {
+                    DisplayExceptionDetails(e);
+                }
+            }
+        }
+
+        private static void DisplayExceptionDetails(Exception e)
+        {
+            System.Console.Write(e.ToString());
+            System.Console.WriteLine();
         }
     }
 }
