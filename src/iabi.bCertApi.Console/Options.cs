5d08bb1cb30ec1663345e758a6c9bb03b211296d
Georg Dangl <dangl@iabi.eu>
2017-10-26 11:21:19 +0200

    Implement CLI options

---
+++ src/iabi.bCertApi.Console/Options.cs
@@ -0,0 +1,29 @@
+﻿using CommandLine;
+using System;
+
+namespace iabi.bCertApi.Console
+{
+    public class Options
+    {
+        [Option('t', "tests", HelpText = "Display all available tests")]
+        public bool ListTests { get; set; }
+
+        [Option('i', "input", HelpText = "Relative or absolute path to an IFC file")]
+        public string InputFilePath { get; set; }
+
+        [Option('k', "apiKey", Required = true, HelpText = "Your b-Cert Api Key")]
+        public string ApiKey { get; set; }
+
+        [Option('j', "json", HelpText = "Output path for the json check result")]
+        public string JsonOutputPath { get; set; }
+
+        [Option('x', "xml", HelpText = "Output path for the xml check result")]
+        public string XmlOutputPath { get; set; }
+
+        [Option('u', "uri", HelpText = "b-Cert Uri", Default = "https://www.b-cert.org")]
+        public string BaseUri { get; set; }
+
+        [Option('m', "mvdId", HelpText = "If specified, tests will be run against this specific MVD instead against the global schema")]
+        public Guid MvdId { get; set; }
+    }
+}
---
+++ src/iabi.bCertApi.Console/OptionsVerifier.cs
@@ -0,0 +1,29 @@
+﻿namespace iabi.bCertApi.Console
+{
+    public static class OptionsVerifier
+    {
+        public static bool IsValid(this Options options)
+        {
+            var shouldListTests = options.ListTests;
+            if (shouldListTests)
+            {
+                return true;
+            }
+            var hasInputAndOutputSpecified = !string.IsNullOrWhiteSpace(options.InputFilePath)
+                && (!string.IsNullOrWhiteSpace(options.XmlOutputPath) || !string.IsNullOrWhiteSpace(options.JsonOutputPath));
+            if (!hasInputAndOutputSpecified)
+            {
+                System.Console.WriteLine("ERROR:");
+                System.Console.WriteLine("Either the Xml or Json output must be specified");
+                return false;
+            }
+            if (options.MvdId == default && !string.IsNullOrWhiteSpace(options.XmlOutputPath))
+            {
+                System.Console.WriteLine("ERROR:");
+                System.Console.WriteLine("Xml reports are only supported for test specific MVDs.");
+                return false;
+            }
+            return true;
+        }
+    }
+}
