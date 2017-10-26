ab9565f4d4238ff98f843decd347f5c7480e526b
Georg Dangl <dangl@iabi.eu>
2017-10-26 11:22:24 +0200

    Add TestCaseLister

---
+++ src/iabi.bCertApi.Console/TestCaseLister.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace iabi.bCertApi.Console
+{
+    public class TestCaseLister
+    {
+        private readonly Options _options;
+        private readonly TestToolService _testToolService;
+
+        public TestCaseLister(Options options, IHttpHandler httpHandler)
+        {
+            _options = options;
+            _testToolService = new TestToolService(httpHandler);
+        }
+
+        public async Task ListTestCasesAsync()
+        {
+            System.Console.WriteLine("Available tests:");
+            var tests = await _testToolService.GetAllTestsAsync();
+            var testPrinter = new TestsPrinter(tests);
+            testPrinter.PrintTests();
+        }
+    }
+}
